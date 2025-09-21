50eb5cc8df85a941ade57ad881871aa45901e771
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2025-09-21 14:43:07 +0200

    Add testing deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-noarg")
     runtimeOnly("org.postgresql:postgresql")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        exclude(group = "org.mockito", module = "mockito-core")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    testImplementation("org.instancio:instancio-core:3.7.0")
+    testImplementation("org.instancio:instancio-junit:3.7.0")
+    testImplementation("io.mockk:mockk:1.13.7")
     testImplementation("org.springframework.security:spring-security-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
@@ -62,4 +69,15 @@ allOpen {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.junit.jupiter") {
+            useVersion("5.10.0")
+        }
+        if (requested.group == "org.junit.platform") {
+            useVersion("1.10.0")
+        }
+    }
 }
